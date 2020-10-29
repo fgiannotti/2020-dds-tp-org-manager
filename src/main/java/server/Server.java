b1e54fc4a4d3882090d1c55accb86fab64ff07e3
fgiannotti <franco.giannotti@mercadolibre.com>
2020-10-29 14:31:01 -0300

    use heroku given port

--- src/main/java/server/Server.java
+++ src/main/java/server/Server.java
@@ -5,7 +5,12 @@ import spark.debug.DebugScreen;
 
 public class Server {
     public static void main(String[] args) {
-        Spark.port(9000);
+        String host = "0.0.0.0";
+        String port = System.getenv("PORT");
+        if (port.equals("")){
+            port = "8000";
+        }
+        Spark.port(Integer.parseInt(port));
         Router.init();
         DebugScreen.enableDebugScreen();
     }
