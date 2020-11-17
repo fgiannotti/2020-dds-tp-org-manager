d7051a3ba37cec1814abb2f2f0fc7be0a969366d
Joaquin Rode <39282494+jtrode@users.noreply.github.com>
2020-11-17 14:15:51 -0300

    Update RepoOperacionesEgresos.java

--- src/main/java/repositorios/RepoOperacionesEgresos.java
+++ src/main/java/repositorios/RepoOperacionesEgresos.java
@@ -19,6 +19,25 @@ public class RepoOperacionesEgresos {
         EntityManagerHelper.createQuery(query).getResultList().forEach((a) -> { operaciones.add((OperacionEgreso)a); });
         return operaciones;
     }
+    
+     public ArrayList<OperacionEgreso> getAllByUser (String username) {
+        String query = "select id,\n" +
+                " cantidadMinimaDePresupuestos,\n" +
+                " criterio,\n" +
+                " descripcion,\n" +
+                " fecha_operacion,\n" +
+                " montoTotal,\n" +
+                " numeroOperacion,\n" +
+                " tipoDocumento,\n" +
+                " comprobante_id,\n" +
+                " ingreso_id,\n" +
+                " medioDePago_id,\n" +
+                " organizacion_id from egresos e JOIN organizaciones o ON e.organizacion_id = o.id JOIN usuarios u ON u.organizacion_id = o.id WHERE u.nombre = '" +
+                        username +  "'";
+        ArrayList<OperacionEgreso> operaciones = new ArrayList<OperacionEgreso>();
+        EntityManagerHelper.createQuery(query).getResultList().forEach((a) -> { operaciones.add((OperacionEgreso)a); });
+        return operaciones;
+    }
 
     public OperacionEgreso find (int id) {
         String query = "from OperacionEgreso";
