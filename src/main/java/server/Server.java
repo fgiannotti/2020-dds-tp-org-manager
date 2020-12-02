5b92e1bbfab8072835dd0d38a89b10c85c4ffcf2
fgiannotti <franco.giannotti@mercadolibre.com>
2020-12-02 15:22:58 -0300

    mas cosas al puerto

--- src/main/java/server/Server.java
+++ src/main/java/server/Server.java
@@ -5,7 +5,13 @@ import spark.debug.DebugScreen;
 
 public class Server {
     public static void main(String[] args) {
-        int port = Integer.parseInt(System.getenv("PORT"));
+        int port;
+        try{
+            port = Integer.parseInt(System.getenv("PORT"));
+        } catch( NumberFormatException e){
+            port=9000;
+        }
+
         System.out.printf("Puerto Encontrado %d",port);
         if (port == 0) {port=9000; }
         Spark.port(port);
