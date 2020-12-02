822c91607e0375e20ce3f02030333351a3bd5698
fgiannotti <franco.giannotti@mercadolibre.com>
2020-12-02 15:08:56 -0300

    port

--- src/main/java/server/Server.java
+++ src/main/java/server/Server.java
@@ -6,6 +6,7 @@ import spark.debug.DebugScreen;
 public class Server {
     public static void main(String[] args) {
         int port = Integer.parseInt(System.getenv("PORT"));
+        if (port == 0) {port=9000; }
         Spark.port(port);
 
         Router.init();
