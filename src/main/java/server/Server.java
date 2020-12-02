8ab522e857542b036fb1c5d282d6e38b0cbb40c3
fgiannotti <franco.giannotti@mercadolibre.com>
2020-12-02 15:29:17 -0300

    server

--- src/main/java/server/Server.java
+++ src/main/java/server/Server.java
@@ -6,14 +6,11 @@ import spark.debug.DebugScreen;
 public class Server {
     public static void main(String[] args) {
         int port;
-        try{
-            port = Integer.parseInt(System.getenv("PORT"));
-        } catch( NumberFormatException e){
-            port=9000;
-        }
+        try{ port = Integer.parseInt(System.getenv("PORT"));
+        } catch( NumberFormatException e){ port=9000; }
 
         System.out.printf("Puerto Encontrado %d",port);
-        if (port == 0) {port=9000; }
+        if (port == 0) { port=9000; }
         Spark.port(port);
 
         Router.init();
