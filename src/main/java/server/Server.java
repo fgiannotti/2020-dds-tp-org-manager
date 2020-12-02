50a438789f29f61df2c9c81a9161ca0bf1f23b87
fgiannotti <franco.giannotti@mercadolibre.com>
2020-12-02 15:13:26 -0300

    log

--- src/main/java/server/Server.java
+++ src/main/java/server/Server.java
@@ -6,6 +6,7 @@ import spark.debug.DebugScreen;
 public class Server {
     public static void main(String[] args) {
         int port = Integer.parseInt(System.getenv("PORT"));
+        System.out.printf("Puerto Encontrado %d",port);
         if (port == 0) {port=9000; }
         Spark.port(port);
 
