9034e30e594ab22335e5b5ef20d1cde8c450d61e
fgiannotti <franco.giannotti@mercadolibre.com>
2020-12-04 16:52:26 -0300

    main tiene que ser static

--- src/main/java/server/Server.java
+++ src/main/java/server/Server.java
@@ -28,7 +28,7 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class Server {
-    public void main(String[] args) {
+    public static void main(String[] args) {
         int port;
         try{ port = Integer.parseInt(System.getenv("PORT"));
         } catch( NumberFormatException e){ port=9000; }
@@ -38,41 +38,42 @@ public class Server {
 
         if (port == 0) { port=9000; }
         Spark.port(port);
-        this.persistirInicial();
+        persistirInicial();
         Router.init();
         DebugScreen.enableDebugScreen();
     }
 
+    public static void persistirInicial(){
 
 
-    public BandejaDeEntrada bandeja;
-    public BandejaDeEntrada bandeja2;
+         BandejaDeEntrada bandeja = new BandejaDeEntrada();
+         BandejaDeEntrada bandeja2 = new BandejaDeEntrada();
 
-    public Empresa organizacion;
-    public OperacionEgreso operacion;
-    public Proveedor proveedor;
-    public MedioDePago medioDePago;
-    public List<Item> items;
-    public List<Articulo> articulos;
-    public List<Proveedor> proveedores = new ArrayList<Proveedor>();
-    public DireccionPostal direccionPostal = new DireccionPostal(new Direccion("calle123",1500,2),new Ciudad("ciudad1"),new Provincia("bs as"),new Pais("arg","AR"));
-    public Organizacion juridica = new Juridica("org-jur","razon",2222,direccionPostal,12,null);
-    public Usuario usuario1 = new User("pepito", "pep's-pass", juridica);
-    public Revisor revisor1 = new Revisor("alfonso","alfi123",juridica,bandeja2);
-    public List<Usuario> usuarios = new ArrayList<Usuario>();
-    public Organizacion juridica2 = new OSC("org-jur","razon",2222,direccionPostal,12);
-    public Organizacion orgBase = new Base("org-base","AA SA", (Juridica) juridica);
-    public Organizacion orgBase2 = new Base("org-base","AA SA", (Juridica) juridica2);
-    public Resultado resultado1;
-    public Resultado resultado2;
-    public Resultado resultado3;
-    public Resultado resultado4;
-    public FiltroPorFecha filtroPorFecha;
-    public FiltroPorEstado filtroPorEstadoNoLeido;
-    public OperacionIngreso ingreso;
+         Empresa organizacion;
+         OperacionEgreso operacion;
+         Proveedor proveedor;
+         MedioDePago medioDePago;
+         List<Item> items = new ArrayList<Item>();;
+         List<Articulo> articulos = new ArrayList<Articulo>();;
+         List<Proveedor> proveedores = new ArrayList<Proveedor>();
+         DireccionPostal direccionPostal = new DireccionPostal(new Direccion("calle123",1500,2),new Ciudad("ciudad1"),new Provincia("bs as"),new Pais("arg","AR"));
+         Organizacion juridica = new Juridica("org-jur","razon",2222,direccionPostal,12,null);
+         Usuario usuario1 = new User("pepito", "pep's-pass", juridica);
+         Revisor revisor1 = new Revisor("alfonso","alfi123",juridica,bandeja2);
+         List<Usuario> usuarios = new ArrayList<Usuario>();
+         Organizacion juridica2 = new OSC("org-jur","razon",2222,direccionPostal,12);
+         Organizacion orgBase = new Base("org-base","AA SA", (Juridica) juridica);
+         Organizacion orgBase2 = new Base("org-base","AA SA", (Juridica) juridica2);
+         Resultado resultado1;
+         Resultado resultado2;
+         Resultado resultado3;
+         Resultado resultado4;
+         FiltroPorFecha filtroPorFecha;
+         FiltroPorEstado filtroPorEstadoNoLeido;
+         OperacionIngreso ingreso;
 
-    public List<Filtro> filtros = new ArrayList<Filtro>();
-    public void setup() {
+         List<Filtro> filtros = new ArrayList<Filtro>();
+
         proveedor = new Proveedor("Nachito deliveries", "123123", "Calle falsa 123");
         double price = 50.0;
         Articulo articulo = new Articulo("CocoWater", (float) price, "Agua de coco 500 ml", proveedor);
@@ -110,7 +111,6 @@ public class Server {
         bandeja = new BandejaDeEntrada(filtros);
         bandeja2 = new BandejaDeEntrada(filtros);
 
-        List<Proveedor> proveedores = new ArrayList<Proveedor>();
         proveedores.add(proveedor);
         proveedor = new Proveedor("jorgito-provides","Docu","calle-falsa123");
         resultado1 = new Resultado(1,proveedores,true,true,true,false, LocalDate.now().minusDays(1),bandeja);
@@ -124,16 +124,13 @@ public class Server {
         bandeja2.guardarResultado(resultado4);
 
         revisor1.setBandejaDeEntrada(bandeja2);
-    }
-    public void persistirInicial(){
-        this.setup();
 
         EntityManagerHelper.beginTransaction();
-        EntityManagerHelper.getEntityManager().persist(this.ingreso);
+        EntityManagerHelper.getEntityManager().persist(ingreso);
         EntityManagerHelper.commit();
 
         EntityManagerHelper.beginTransaction();
-        EntityManagerHelper.getEntityManager().persist(this.operacion);
+        EntityManagerHelper.getEntityManager().persist(operacion);
         EntityManagerHelper.commit();
     }
 }
